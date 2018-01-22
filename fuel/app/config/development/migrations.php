1f0236c2c15ca01bb8e43c79821f8f8b2408ae6c
fercrefe <fernando_cremades_apps1m1617@cev.com>
2018-01-22 08:23:04 -0500

    firsasdtcommit

---
+++ fuel/app/config/development/migrations.php
@@ -0,0 +1,27 @@
+<?php
+return array(
+  'version' => 
+  array(
+    'app' => 
+    array(
+      'default' => 
+      array(
+        0 => '001_users',
+        1 => '002_places',
+        2 => '003_objects',
+        3 => '004_valuations',
+        4 => '005_search',
+        5 => '006_buy',
+        6 => '007_favorites',
+      ),
+    ),
+    'module' => 
+    array(
+    ),
+    'package' => 
+    array(
+    ),
+  ),
+  'folder' => 'migrations/',
+  'table' => 'migration',
+);
---
+++ fuel/app/logs/2018/01/22.php
@@ -0,0 +1,11 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2018-01-22 13:11:10 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-22 13:11:37 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-22 13:11:52 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-22 13:11:53 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-22 13:11:54 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-22 13:11:54 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-22 13:11:54 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-22 13:11:56 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-22 13:13:27 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
